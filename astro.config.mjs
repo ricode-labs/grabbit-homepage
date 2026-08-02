5e6ca02f253e09807eb1a0c3945bec54b3bc1612
Siuhaa <leesiuhaoct23@gmail.com>
2026-08-02 13:01:13 +0900

    feat: workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const base = process.env.GITHUB_PAGES === 'true' && repoName ? `/${repoName}/` : '/';
+
+export default defineConfig({
+	base,
+});
